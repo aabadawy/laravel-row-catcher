917cd125b2a3fd904422aa760b1eedb0e40c9139
aABadawy <ahmedbadawy.fcai@gmail.com>
2022-02-18 21:20:24 +0200

    wip

--- src/row_catcher.php
+++ src/row_catcher.php
@@ -1,12 +1,12 @@
 <?php
 return [
     /*
-     * ----------------------------------------------------------
-     * the default minmum number of failures to start take action
-     * ----------------------------------------------------------
-     | Here we definig the minmum number of failures before fire egistered events
-     | so may after catch five failures, fire event which will be responisible to
-     | Push some helpfull info to admins about that
+     * ----------------------------------------------------------------------------
+     * the default minimum number of failures to start take action
+     * ----------------------------------------------------------------------------
+     | Here we defining the minimum number of failures before fire registered events
+     | so may after catch five failures, fire event which will be responsible to
+     | Push some helpfully info to admins about that
      */
     'min_critical_failures'  => 5,
 ];
