c8923c703e831e482964650a3853e48f9b79323e
aABadawy <ahmedbadawy.fcai@gmail.com>
2022-02-19 13:05:51 +0200

    catch rows in one single array instead of using objects

--- src/RowCatcher.php
+++ src/RowCatcher.php
@@ -18,17 +18,27 @@ class RowCatcher implements RowCatcherContract
 
     protected array|\Countable $rows;
 
+    /**
+     * init start catching
+     * @param array|\Countable $rows
+     * @return $this
+     */
     public function startCatching(array|\Countable $rows):self
     {
         $this->rows = $rows;
 
-        $this->totalRows = (is_countable($rows)) ? count($rows) : $rows;
+        $this->totalRows = count($rows);
 
         $this->init();
 
         return $this;
     }
 
+    /**
+     * loop through rows with passed callable
+     * @param callable $callable
+     * @return void
+     */
     public function each(callable $callable):void
     {
         foreach ($this->rows as $row) {
@@ -41,51 +51,96 @@ class RowCatcher implements RowCatcherContract
         }
     }
 
+    /**
+     * clear the catching
+     * @return void
+     */
     public function endCatching():void
     {
         $this->init();
     }
 
+    /**
+     * get all failure rows
+     * @return Collection
+     */
     public function getFailures():Collection
     {
         return $this->failures;
     }
 
+    /**
+     * get all success rows
+     * @return Collection
+     */
     public function getSuccesses():Collection
     {
         return $this->successes;
     }
-    
+
+    /**
+     * catch failure row and the exception
+     * @param \Throwable $throwable
+     * @param $row
+     * @return void
+     */
     public function catchFailure(\Throwable $throwable,$row = null): void
     {
-        $this->failures->add(new FailureRow($throwable,$row));
+        $failure_row = $this->failureRow($throwable,$row);
+        $this->failures->add($failure_row);
     }
 
+    /**
+     * catch success row
+     * @param $row
+     * @return void
+     */
     public function catchSuccess($row = null):void
     {
-        $this->successes->add(new SuccessRow($row));
+        $success_row = $this->successRow($row);
+        $this->successes->add($success_row);
     }
 
+    /**
+     * get total number of failures
+     * @return int
+     */
     public function numberOfFailures():int
     {
         return $this->failures->count();
     }
 
+    /**
+     * get total number of successes
+     * @return int
+     */
     public function numberOfSuccesses():int
     {
         return $this->successes->count();
     }
 
+    /**
+     * determine all passed rows are successful or not
+     * @return bool
+     */
     public function allSuccess():bool
     {
         return $this->numberOfSuccesses() == $this->totalRows;
     }
 
+    /**
+     * determine all passed rows are failed or not
+     * @return bool
+     */
     public function allFailed(): bool
     {
         return $this->numberOfFailures() == $this->totalRows;
     }
 
+    /**
+     * init empty failures and successes collections
+     * @return void
+     */
     private function init()
     {
         $this->initEmptyFailures();
@@ -102,5 +157,19 @@ class RowCatcher implements RowCatcherContract
         $this->successes    = new Collection();
     }
 
+    /**
+     * @param \Throwable $throwable
+     * @param $row
+     * @return array
+     */
+    private function failureRow(\Throwable $throwable,$row = null)
+    {
+        return ['exception' => $throwable,'row' => $row];
+    }
+
+    private function successRow($row = null)
+    {
+        return ['row' => $row];
+    }
     //TODO fire event after passed number of failures and passed number of successes if needed
 }
